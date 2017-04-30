06b6ad5c6c625da81ab45d1e77eb13abe6f4d92e
forkerino <pieterstokkink@gmail.com>
2017-04-30 23:29:12 +0200

    added JSON mocking

--- buildscripts/srcServer.js
+++ buildscripts/srcServer.js
@@ -18,6 +18,10 @@ app.get('/', function(req, res){
     res.sendFile(path.join(__dirname,'../src/index.html'));
 });
 
+app.get('/users', function(req, res){
+    res.json({"users":[{"id":16616451,"firstName":"Vilma","lastName":"Cummerata","email":"Bartholome72@yahoo.com"},{"id":99442990,"firstName":"Lavinia","lastName":"Powlowski","email":"Josephine76@gmail.com"},{"id":60866635,"firstName":"Carleton","lastName":"Cormier","email":"Bart75@hotmail.com"},{"id":96282023,"firstName":"Brody","lastName":"Cole","email":"Nicklaus51@hotmail.com"},{"id":18134034,"firstName":"Felix","lastName":"Yundt","email":"Adriana_Trantow97@yahoo.com"}]});
+});
+
 app.listen(port,function(err){
     if (err){
         console.log(err);
